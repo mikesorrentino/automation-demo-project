7b2d3c6493b7721ca9f849edb59aa9e563f3ce91
filipe-gomes <filipe.gomes@globalpay.com>
2025-07-29 15:52:22 -0400

    Upgraded versions

--- src/main/java/com/springselenium/automation/config/WebDriverConfig.java
+++ src/main/java/com/springselenium/automation/config/WebDriverConfig.java
@@ -1,16 +1,18 @@
 package com.springselenium.automation.config;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class WebDriverConfig {
@@ -21,20 +23,20 @@ public class WebDriverConfig {
 	@Bean
 	@ConditionalOnMissingBean
 	public WebDriver chromeDriver() {
-		WebDriverManager.chromedriver().setup();
-		return new ChromeDriver();
-	}
-	
-	@Bean
-	@ConditionalOnProperty(name = "browser", havingValue = "firefox")
-	public WebDriver firefoxDriver() {
-		WebDriverManager.firefoxdriver().setup();
-		return new FirefoxDriver();
+		Map<String, Object> chromePrefs = new HashMap<>();
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setExperimentalOption("prefs", chromePrefs);
+		chromeOptions.addArguments("--remote-allow-origins=*");
+		chromeOptions.addArguments("start-maximized");
+		chromeOptions.setExperimentalOption("prefs", chromePrefs);
+
+		WebDriverManager.chromedriver().clearDriverCache().setup();
+		return new ChromeDriver(chromeOptions);
 	}
 	
 	@Bean
 	public WebDriverWait webDriverWait(WebDriver driver) {
-		return new WebDriverWait(driver, timeout);
+		return new WebDriverWait(driver, Duration.ofSeconds(timeout));
 	}
 
 }
--- src/test/java/com/springselenium/automation/cucumber/features/GoogleSearch.feature
+++ src/test/java/com/springselenium/automation/cucumber/features/GoogleSearch.feature
@@ -1,7 +1,6 @@
 Feature: As a user, I want to search for Ronaldo on Google
 
   Scenario: I search for Ronaldo and land on the results page
-
     Given I am on the google page
     When I search for "Ronaldo"
     And I click the search button
